511862c22d0ba5b865517be854d379677e1baee8
Anirudha Havaldar <anirudha.hero@gmail.com>
2026-05-12 11:09:13 +0530

    Fetch and filter restaurant data in Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,30 +1,63 @@
-import { useState } from "react";
-import { resObj } from "../helper/utils";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(
-    resObj.data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants,
-  );
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setfilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://corsproxy.io/?https://namastedev.com/api/v1/listRestaurants")
+    console.log(data)
+    const response = await data.json();
+    setListOfRestaurants(response?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setfilteredRestaurants(response?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(response)
+  }
+
+
 
   const filterRestaurants = () => {
     const filteredRestaurants = listOfRestaurants.filter(
-      (res) => res.info.avgRating >= 4,
+      (res) => res.info.avgRating >= 4.5,
     );
-    setListOfRestaurants(filteredRestaurants);
+    setfilteredRestaurants(filteredRestaurants);
   };
+
+  const filterRes = () => {
+    let searchRes = listOfRestaurants.filter(res => res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+    console.log(searchRes)
+
+    setfilteredRestaurants(searchRes)
+
+  }
+
+
+  if(listOfRestaurants.length === 0) {
+    return (
+      <h1> Loadinnnnnnnnnggggggggggggggggggg </h1>
+    )
+  }
 
   return (
     <div className="body">
       <div className="search">
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" value={searchText} onChange={(e)=> {
+          setSearchText(e.target.value)
+        }}/>
+
+        <button onClick={filterRes}>Search</button>
         <button className="top-restro-btn" onClick={filterRestaurants}>
           {" "}
           Top Rated Restrurant{" "}
         </button>
       </div>
       <div className="restaurant-container">
-        {listOfRestaurants.map((restaurant) => {
+        {filteredRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
